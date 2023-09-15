3890d001997d7ea248681eeb772df4b8144d9c42
shohijahonaxmetov <shohijahonaxmetov@gmail.com>
2023-09-15 15:38:29 +0500

    git error fix

--- app/Models/Equipment.php
+++ app/Models/Equipment.php
@@ -20,13 +20,18 @@ class Equipment extends Model
         return $this->belongsTo(TypeEquipment::class);
     }
 
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
     public function planRemonts(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
         return $this->hasMany(PlanRemont::class);
     }
 
-    public function department()
+    public function repairApplications(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
-        return $this->belongsTo(Department::class);
+        return $this->hasMany(RepairApplication::class);
     }
 }
