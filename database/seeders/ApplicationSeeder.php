c33269ef55c11dad0b64d47b4722d84c5ab385ee
Raxim aka <User@center6.h.tdtu.uz>
2023-09-21 16:29:28 +0500

    seeder fix

--- database/seeders/ApplicationSeeder.php
+++ database/seeders/ApplicationSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\Application;
 use App\Models\PlanRemont;
+use App\Models\RepairApplication;
+;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,13 +21,45 @@ class ApplicationSeeder extends Seeder
 		foreach($items as $item) {
 			$item->delete();
 		}
+		$items = RepairApplication::all();
+		foreach($items as $item) {
+			$item->delete();
+		}
 
+		$curreny_date = date('Y-m-d');
+
+		// Заявки на ремонт
     	for($i=0; $i<500; $i++) {
     		$required_quantity = rand(51,99);
     		$plan_remont_id = rand(1,200);
     		$plan_remont = PlanRemont::find($plan_remont_id);
     		$warehouse_quantity = $required_quantity - rand(1,50);
-    		$warehouse_date = date('Y-m-d', strtotime($plan_remont->remont_begin.'-'.rand(20,50).' day'));
+			// Проверить дату заявки и склада
+			if ($plan_remont->remont_begin < $curreny_date) {
+				$warehouse_date = date('Y-m-d', strtotime($plan_remont->remont_begin.'-'.rand(20,50).' day'));
+			} else {
+				$warehouse_date = date('Y-m-d', strtotime($curreny_date.'-'.rand(20,50).' day'));
+			}
+
+			// Вставка заявки на ремонт
+			// Если заявка на этот ремонт
+			//$application2 ;
+			//var_dump($application2);
+			//die();
+			if (RepairApplication::where('plan_remont_id', $plan_remont_id)->get()->count() > 0) {
+				// есть
+				$application2 = RepairApplication::where('plan_remont_id', $plan_remont_id)->latest()->first();
+				$warehouse_date = $application2->application_date;
+			} else {
+				// нет
+				$data_app = [
+					'application_date' => $warehouse_date,
+					'equipment_id' => $plan_remont->equipment_id,
+					'plan_remont_id' => $plan_remont_id,
+				];
+				RepairApplication::create($data_app);
+			}
+    		
     		$data = [
     			'id' => $i+1,
     			'plan_remont_id' => $plan_remont_id,
@@ -74,11 +108,13 @@ class ApplicationSeeder extends Seeder
 
 		for($i=0; $i<1000; $i++) {
     		$required_quantity = rand(51,99);
-    		$plan_remont_id = rand(1,200);
+			// $plan_remont_id = rand(1,200);
+    		$plan_remont_id = rand(1,100);
     		$plan_remont = PlanRemont::find($plan_remont_id);
     		$warehouse_quantity = $required_quantity - rand(1,50);
     		// $warehouse_date = date('Y-m-d', strtotime($plan_remont->remont_begin.'-'.rand(20,50).' day'));
-    		$warehouse_date = '2022-12-'.rand(01,28);
+    		 $warehouse_date = '2022-12-'.rand(01,28);
+
     		$data = [
     			'id' => $i+1+1000,
     			'plan_remont_id' => $plan_remont_id,
--- database/seeders/PlanRemontSeeder.php
+++ database/seeders/PlanRemontSeeder.php
@@ -13,7 +13,11 @@ class PlanRemontSeeder extends Seeder
      */
     public function run(): void
     {
-        for($i=0; $i<200; $i++) {
+        $items = PlanRemont::all();
+		foreach($items as $item) {
+			$item->delete();
+		}
+/*        for($i=0; $i<200; $i++) {
             $data['id'] = $i+1;
         	$data['equipment_id'] = rand(1,21);
         	$data['remont_type_id'] = rand(1,4);
@@ -21,6 +25,86 @@ class PlanRemontSeeder extends Seeder
         	$data['remont_end'] = date('Y-m-d', strtotime($data['remont_begin'].'+ '.rand(1, 50).' days'));
 
         	PlanRemont::create($data);
+        } */
+
+        for($i=0; $i<150; $i++) {
+            $data['id'] = $i+1;
+        	$data['equipment_id'] = rand(1, 30);
+        	$data['remont_type_id'] = rand(1, 4);
+            if ($data['remont_type_id'] == 1) {
+                if ($plan_remont = PlanRemont::where('equipment_id', $data['equipment_id'])
+                    ->where('remont_type_id', 1)->get()->count() > 0) {
+                        $data['remont_type_id'] = 4;
+                }
+            }
+
+        	$data['remont_begin'] = '2023-'.rand(03, 12).'-'.rand(01,28);
+            if ($data['remont_type_id'] == 4) {
+                $data['remont_end'] = date('Y-m-d', strtotime($data['remont_begin'].'+ '.rand(2, 4).' days'));    
+            } else if ($data['remont_type_id'] == 3) {
+                $data['remont_end'] = date('Y-m-d', strtotime($data['remont_begin'].'+ '.rand(5, 9).' days'));
+            } else if ($data['remont_type_id'] == 2) {
+                $data['remont_end'] = date('Y-m-d', strtotime($data['remont_begin'].'+ '.rand(10, 19).' days'));
+            } else {
+                $data['remont_end'] = date('Y-m-d', strtotime($data['remont_begin'].'+ '.rand(20, 40).' days'));
+            }
+            // 
+            if ($plan_remont = PlanRemont::where('equipment_id', $data['equipment_id'])
+                    ->whereBetween('remont_begin', [$data['remont_begin'], $data['remont_end']])->get()->count() > 0) {
+                // повтор
+                $data['remont_begin'] = '2023-'.rand(03, 12).'-'.rand(01,28);
+                if ($data['remont_type_id'] == 4) {
+                    $data['remont_end'] = date('Y-m-d', strtotime($data['remont_begin'].'+ '.rand(2, 4).' days'));    
+                } else if ($data['remont_type_id'] == 3) {
+                    $data['remont_end'] = date('Y-m-d', strtotime($data['remont_begin'].'+ '.rand(5, 9).' days'));
+                } else if ($data['remont_type_id'] == 2) {
+                    $data['remont_end'] = date('Y-m-d', strtotime($data['remont_begin'].'+ '.rand(10, 19).' days'));
+                } else {
+                    $data['remont_end'] = date('Y-m-d', strtotime($data['remont_begin'].'+ '.rand(20, 40).' days'));
+                }
+            }
+
+        	PlanRemont::create($data);
         }
+        for($i=150; $i<200; $i++) {
+            $data['id'] = $i+1;
+        	$data['equipment_id'] = rand(1, 30);
+        	$data['remont_type_id'] = rand(1,4);
+            if ($data['remont_type_id'] == 1) {
+                if ($plan_remont = PlanRemont::where('equipment_id', $data['equipment_id'])
+                    ->where('remont_type_id', 1)->get()->count() > 0) {
+                        $data['remont_type_id'] = 4;
+                }
+            }
+
+        	$data['remont_begin'] = '2024-'.rand(01, 03).'-'.rand(01,28);
+            if ($data['remont_type_id'] == 4) {
+                $data['remont_end'] = date('Y-m-d', strtotime($data['remont_begin'].'+ '.rand(2, 4).' days'));    
+            } else if ($data['remont_type_id'] == 3) {
+                $data['remont_end'] = date('Y-m-d', strtotime($data['remont_begin'].'+ '.rand(5, 9).' days'));
+            } else if ($data['remont_type_id'] == 2) {
+                $data['remont_end'] = date('Y-m-d', strtotime($data['remont_begin'].'+ '.rand(10, 19).' days'));
+            } else {
+                $data['remont_end'] = date('Y-m-d', strtotime($data['remont_begin'].'+ '.rand(20, 40).' days'));
+            }
+
+            if ($plan_remont = PlanRemont::where('equipment_id', $data['equipment_id'])
+                    ->whereBetween('remont_begin', [$data['remont_begin'], $data['remont_end']])->get()->count() > 0) {
+                // повтор
+                $data['remont_begin'] = '2024-'.rand(01, 03).'-'.rand(01,28);
+                if ($data['remont_type_id'] == 4) {
+                    $data['remont_end'] = date('Y-m-d', strtotime($data['remont_begin'].'+ '.rand(2, 4).' days'));    
+                } else if ($data['remont_type_id'] == 3) {
+                    $data['remont_end'] = date('Y-m-d', strtotime($data['remont_begin'].'+ '.rand(5, 9).' days'));
+                } else if ($data['remont_type_id'] == 2) {
+                    $data['remont_end'] = date('Y-m-d', strtotime($data['remont_begin'].'+ '.rand(10, 19).' days'));
+                } else {
+                    $data['remont_end'] = date('Y-m-d', strtotime($data['remont_begin'].'+ '.rand(20, 40).' days'));
+                }
+            }
+
+        	PlanRemont::create($data);
+        }
+
     }
 }
