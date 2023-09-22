0d1a4b6ffde11f6d7b9e36622d7849ce48ae8a01
Raxim aka <User@center6.h.tdtu.uz>
2023-09-22 16:27:34 +0500

    git seed fix1

--- database/seeders/OrderResourceSeeder.php
+++ database/seeders/OrderResourceSeeder.php
@@ -19,6 +19,7 @@ class OrderResourceSeeder extends Seeder
 			$item->delete();
 		}
 
+/* Оригинал
         for($i=0; $i<2000; $i++) {
         	$application_id = rand(1,1999);
         	$application = Application::find($application_id);
@@ -97,7 +98,108 @@ class OrderResourceSeeder extends Seeder
         	$data['execution_statuse_id'] = $this->setStatus($data);
 
         	OrderResource::create($data);
-        }
+        } */
+		
+
+        for($i=0; $i<2000; $i++) {
+        	$application_id = $i + 1;
+        	$application = Application::find($application_id);
+			
+            $order_date = date('Y-m-d', strtotime($application->application_date.'+'.rand(0,10).' day')); // заказ - 60
+
+            $contract_number = '№ '.rand(1564,5486451);
+        	$contract_date = date('Y-m-d', strtotime($order_date.'+'.rand(1,10).' days')); // контракт - 60
+        	$local_foreign = rand(1,2);
+        	$date_manufacture_contract = date('Y-m-d', strtotime($contract_date.'+'.rand(1,30).' days'));
+        	$date_manufacture_fact = date('Y-m-d', strtotime($contract_date.'+'.rand(0,30).' days'));
+        	$customs_date_receipt = null;
+        	$customs_date_exit = null;
+        	$date_delivery_object = date('Y-m-d', strtotime($date_manufacture_fact.'+'.rand(0,10).' days')); // 15
+        	if($local_foreign == 2) {
+        		$customs_date_receipt = date('Y-m-d', strtotime($date_manufacture_fact.'+'.rand(0, 4).' days'));
+        		$customs_date_exit = date('Y-m-d', strtotime($customs_date_receipt.'+'.rand(0, 7).' days'));
+        		$date_delivery_object = date('Y-m-d', strtotime($customs_date_exit.'+'.rand(0, 10).' days'));  //30
+        	}
+
+            $day_sum = (strtotime(date('Y-m-d')) - strtotime($application->remont_begin)) / (24*60*60);
+			/* или
+            $day_sum = (strtotime($application->delivery_date) - strtotime($order_date)) / (24*60*60);
+			*/
+			
+            if ($day_sum > 60 ) {
+//                if ($day_sum > 30 && $application->application_type == 1) {
+                $rand_status = 7;
+                $contract_number = $day_sum;
+            } else {
+                $rand_status = rand(2,7);
+                $contract_number = $day_sum;
+            }
+
+            
+			$order_number = '№ '.rand(1564,5486451);
+
+            switch ($rand_status) {
+                case 1:
+					$order_number = null;
+					$order_date = null;
+                    $contract_number = null;
+                    $contract_date = null;
+                    $local_foreign = null;
+                    $date_manufacture_contract = null;
+                    $date_manufacture_fact = null;
+                    $customs_date_receipt = null;
+                    $customs_date_exit = null;
+                    $date_delivery_object = null;
+                    break;
+                case 2:
+                    $contract_number = null;
+                    $contract_date = null;
+                    $local_foreign = null;
+                    $date_manufacture_contract = null;
+                    $date_manufacture_fact = null;
+                    $customs_date_receipt = null;
+                    $customs_date_exit = null;
+                    $date_delivery_object = null;
+                    break;
+                case 3:
+                    $date_manufacture_fact = null;
+                    $customs_date_receipt = null;
+                    $customs_date_exit = null;
+                    $date_delivery_object = null;
+                    break;
+                case 4:
+                    $customs_date_receipt = null;
+                    $customs_date_exit = null;
+                    $date_delivery_object = null;
+                    break;
+                case 5:
+                    $customs_date_exit = null;
+                    $date_delivery_object = null;
+                    break;
+                case 6:
+                    $date_delivery_object = null;
+                    break;
+            }
+
+        	$data = [
+        		'application_id' => $application_id,
+        		'order_number' => $order_number,
+        		'order_date' => $order_date,
+        		'order_quantity' => $application->declared_quantity,
+        		'contract_number' => $contract_number,
+        		'contract_date' => $contract_date,
+        		'local_foreign' => $local_foreign,
+        		'date_manufacture_contract' => $date_manufacture_contract,
+        		'date_manufacture_fact' => $date_manufacture_fact,
+        		'customs_date_receipt' => $customs_date_receipt,
+        		'customs_date_exit' => $customs_date_exit,
+        		'date_delivery_object' => $date_delivery_object,
+        	];
+        	$data['execution_statuse_id'] = $this->setStatus($data);
+
+        	OrderResource::create($data);
+        } 
+		
     }
 
     public function setStatus($data)
