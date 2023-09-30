9b00b224514ff05a6ae642ad9dd7c421f6a0102b
shohijahonaxmetov <shohijahonaxmetov@gmail.com>
2023-09-30 14:50:48 +0500

    delete comments from home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -109,7 +109,7 @@
 
     <div class="row">
         <div class="col-12">
-            
+
             <div class="card">
                 <div class="card-header">
 
@@ -193,71 +193,6 @@
 @endsection
 
 @section('scripts')
-<!-- for graph -->
-<!-- <script src="https://d3js.org/d3.v3.min.js"></script> -->
-
-<script>
-
-// var width = 1920,
-//     height = 1000
-
-// var svg = d3.select("#for_graph").append("svg")
-//     .attr("width", width)
-//     .attr("height", height);
-
-// var force = d3.layout.force()
-//     .gravity(0.05)
-//     .distance(300)
-//     .charge(-300)
-//     .size([width, height]);
-
-// let json = @json($forGraph);
-
-
-
-// force
-//   .nodes(json.nodes)
-//   .links(json.links)
-//   .start();
-
-// var link = svg.selectAll(".link")
-//   .data(json.links)
-// .enter().append("line")
-//   .attr("class", "graph_link");
-
-// var node = svg.selectAll(".node")
-//   .data(json.nodes)
-// .enter().append("g")
-//   .attr("class", "graph_node")
-//   .call(force.drag);
-
-// // node.append("image")
-// //   .attr("xlink:href", "https://github.com/favicon.ico")
-// //   .attr("x", -8)
-// //   .attr("y", -8)
-// //   .attr("width", 16)
-// //   .attr("height", 16);
-
-// node.append("circle")
-//     .attr("r", 12);
-
-// node.append("text")
-//   .attr("dx", 12)
-//   .attr("dy", ".35em")
-//   .text(function(d) { return d.name });
-
-// force.on("tick", function() {
-//     link.attr("x1", function(d) { return d.source.x; })
-//         .attr("y1", function(d) { return d.source.y; })
-//         .attr("x2", function(d) { return d.target.x; })
-//         .attr("y2", function(d) { return d.target.y; });
-
-//     node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-// });
-
-</script>
-<!-- for graph end -->
-
 <!-- calendar scripts -->
 <script src="https://code.jquery.com/jquery-3.6.2.min.js" integrity="sha256-2krYZKh//PcchRtd+H+VyyQoZ/e3EcrkxhM8ycwASPA=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
